5a4fd55547c73027e02d1bd0e3c2fa8662a432e4
Jairo Jair Toro Novellis <jtnovellis88@gmail.com>
2023-02-01 00:35:36 -0500

    feat: add context

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,99 @@
+import { useMutation, UseMutationResult } from '@tanstack/react-query'
+import { createContext, useState } from 'react'
+import { User } from '../../types'
+import { api } from '../lib/api'
+
+export type AuthContext = {
+  user: User
+  token: string
+  signup: UseMutationResult<
+    {
+      name: string
+      email: string
+      role: string
+      token: string
+    },
+    unknown,
+    User,
+    unknown
+  >
+  login: UseMutationResult<
+    {
+      name: string
+      email: string
+      role: string
+      token: string
+    },
+    unknown,
+    {
+      email: string
+      password: string
+    },
+    unknown
+  >
+}
+
+export const Context = createContext<AuthContext | null>(null)
+
+type AuthProviderProps = {
+  children: React.ReactNode
+}
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User>({
+    name: '',
+    email: '',
+    password: '',
+    role: '',
+  })
+  const [token, setToken] = useState<string>('token')
+
+  const signup = useMutation({
+    mutationFn: (user: User) => {
+      return api.post('/auth/local/signup', user).then(
+        res =>
+          res.data as {
+            name: string
+            email: string
+            role: string
+            token: string
+          }
+      )
+    },
+    onSuccess(data) {
+      setToken(data.token)
+      setUser({
+        email: data.email,
+        role: data.role,
+        name: data.name,
+      })
+    },
+  })
+
+  const login = useMutation({
+    mutationFn: ({ email, password }: { email: string; password: string }) => {
+      return api.post('/auth/local/login', { email, password }).then(
+        res =>
+          res.data as {
+            name: string
+            email: string
+            role: string
+            token: string
+          }
+      )
+    },
+    onSuccess(data) {
+      setToken(data.token)
+      setUser({
+        email: data.email,
+        role: data.role,
+        name: data.name,
+      })
+    },
+  })
+  return (
+    <Context.Provider value={{ user, token, signup, login }}>
+      {children}
+    </Context.Provider>
+  )
+}
--- src/router.tsx
+++ src/router.tsx
@@ -1,25 +1,31 @@
 import { createBrowserRouter } from 'react-router-dom'
-import Login from './components/Login'
-import SignUp from './components/SignUp'
+import Login from './pages/Login'
+import SignUp from './pages/SignUp'
 import Dashboard from './pages/Dashboard'
 import HomePage from './pages/HomePage'
 import AuthLayout from './pages/layouts/AuthLayout'
 import RootLayout from './pages/layouts/RootLayout'
+import AppProvider from './components/AppProvider'
 
 export const router = createBrowserRouter([
   {
-    path: '/',
-    element: <RootLayout />,
+    element: <AppProvider />,
     children: [
-      { index: true, element: <HomePage /> },
-      { path: 'dashboard', element: <Dashboard /> },
-    ],
-  },
-  {
-    element: <AuthLayout />,
-    children: [
-      { path: 'login', element: <Login /> },
-      { path: 'signup', element: <SignUp /> },
+      {
+        path: '/',
+        element: <RootLayout />,
+        children: [
+          { index: true, element: <HomePage /> },
+          { path: 'dashboard', element: <Dashboard /> },
+        ],
+      },
+      {
+        element: <AuthLayout />,
+        children: [
+          { path: 'login', element: <Login /> },
+          { path: 'signup', element: <SignUp /> },
+        ],
+      },
     ],
   },
 ])
